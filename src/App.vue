2c8fa91a1f49aa8d3b7f208c276d9432c16ce17a
Asher Norland <asher.norland@gmail.com>
2019-07-05 20:58:35 -0400

    Cleanup + Flexbox

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,22 @@
 <template lang="pug">
-<router-view/>
+
+router-view.app
+
 </template>
+
+<style lang="less">
+
+.app{
+
+    display: flex;
+    min-height: 100vh;
+
+}
+
+body {
+
+    margin: 0;
+
+}
+
+</style>
