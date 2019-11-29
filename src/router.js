b94d16f7434b1dd8b80d32b92ca0eef8840f77bb
Asher Norland <asher.norland@gmail.com>
2019-11-29 11:54:58 -0500

    Back to history mode

--- src/router.js
+++ src/router.js
@@ -10,7 +10,7 @@ const Reviews = () => import('./views/Reviews');
 Vue.use(Router);
 
 export default new Router({
-    mode: 'hash',
+    mode: 'history',
     base: process.env.BASE_URL,
     routes: [
         {
