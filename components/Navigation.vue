05bf4d86816a6f4b5b2b6539a5765969fdd21e2f
Asher Norland <asher.norland@gmail.com>
2019-12-28 16:52:22 -0500

    Update Toolbar

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,26 +1,21 @@
 <template lang="pug">
 
-v-toolbar
+v-toolbar(dense)
     v-toolbar-title(to="/" nuxt) Asher Norland
         v-avatar
             v-img(src="favicon.ico" width="32" height="32" alt="Icon")
     //{{ emoji }}
     v-spacer
     v-toolbar-items.hidden-sm-and-down
-        v-btn(v-for="(item, index) in menu"
-          :key="index"
-          :to="item.url"
-          text
-          nuxt) {{ item.title }}
+        v-btn(v-for="(item, index) in menu" :key="index" :to="item.url" text nuxt) {{ item.title }}
 
-    v-menu.hidden-md-and-up(offset-y)
-        template(v-slot:activator="{on}")
-        v-btn(v-on="on") DropDown
+    v-menu
+        template(v-slot:activator="{ on }")
+            v-btn.hidden-md-and-up(v-on="on")
+                v-icon mdi-dots-vertical
         v-list
-            v-list-tile(v-for="(item, index) in menu" :key="index")
-                v-list-tile-content
-                    v-list-tile-title {{item.title}}
-
+            v-list-item(v-for="(item, index) in menu" :key="index" :to="item.url" nuxt)
+                v-list-item-title {{item.title}}
 </template>
 
 <script>
