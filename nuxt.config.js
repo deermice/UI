ff0680d6ee9b90854c55cb4774cfc33813b7887f
Asher Norland <asher.norland@gmail.com>
2020-08-08 17:15:15 -0400

    Dep. Updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,23 @@
+import colors from 'vuetify/es5/util/colors';
 
 export default {
+    /*
+    ** Nuxt rendering mode
+    ** See https://nuxtjs.org/api/configuration-mode
+    */
     mode: 'spa',
     /*
+    ** Nuxt target
+    ** See https://nuxtjs.org/api/configuration-target
+    */
+    target: 'static',
+    /*
     ** Headers of the page
+    ** See https://nuxtjs.org/api/configuration-head
     */
     head: {
-        title: 'Asher Norland',
+        titleTemplate: '%s - ' + process.env.npm_package_name,
+        title: process.env.npm_package_name || '',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -17,14 +29,11 @@ export default {
     },
     /*
     ** Customize the server address
+    ** https://nuxtjs.org/guides/configuration-glossary/configuration-server
     */
     server: {
-        host: '0.0.0.0' // default: localhost
+        host: '0.0.0.0'
     },
-    /*
-    ** Customize the progress-bar color
-    */
-    loading: { color: '#fff' },
     /*
     ** Global CSS
     */
@@ -32,41 +41,74 @@ export default {
     ],
     /*
     ** Plugins to load before mounting the App
+    ** https://nuxtjs.org/guide/plugins
     */
     plugins: [
     ],
+    /*
+    ** Auto import components
+    ** See https://nuxtjs.org/api/configuration-components
+    */
+    components: true,
     /*
     ** Nuxt.js dev-modules
     */
     buildModules: [
         // Doc: https://github.com/nuxt-community/eslint-module
-        '@nuxtjs/eslint-module'
+        '@nuxtjs/eslint-module',
+        '@nuxtjs/vuetify'
     ],
     /*
     ** Nuxt.js modules
     */
     modules: [
-        '@nuxtjs/vuetify',
         // Doc: https://axios.nuxtjs.org/usage
-        '@nuxtjs/axios'
+        '@nuxtjs/axios',
+        // Doc: https://github.com/nuxt/content
+        '@nuxt/content'
     ],
     /*
     ** Axios module configuration
     ** See https://axios.nuxtjs.org/options
     */
-    axios: {
+    axios: {},
+    /*
+    ** Content module configuration
+    ** See https://content.nuxtjs.org/configuration
+    */
+    content: {},
+    /*
+    ** vuetify module configuration
+    ** https://github.com/nuxt-community/vuetify-module
+    */
+    vuetify: {
+        customVariables: ['~/assets/variables.less'],
+        theme: {
+            dark: false,
+            themes: {
+                dark: {
+                    primary: colors.blue.darken2,
+                    accent: colors.grey.darken3,
+                    secondary: colors.amber.darken3,
+                    info: colors.teal.lighten1,
+                    warning: colors.amber.base,
+                    error: colors.deepOrange.accent4,
+                    success: colors.green.accent3
+                }
+            }
+        }
     },
     /*
     ** Build configuration
+    ** See https://nuxtjs.org/api/configuration-build/
+    */
+    build: {
+    },
+    /*
+    ** generate configuration
+    ** See https://nuxtjs.org/api/configuration-generate
     */
     generate: {
         fallback: true
-    },
-    build: {
-        /*
-        ** You can extend webpack config here
-        */
-        extend (config, ctx) {
-        }
     }
 };
