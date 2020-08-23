09dfdaf2a0a6df9153d6ba5e1f4051b3e181e23a
Asher Norland <asher.norland@gmail.com>
2020-08-23 19:22:55 -0400

    Header

--- components/Header.vue
+++ components/Header.vue
@@ -1,41 +1,121 @@
 <template  lang="pug">
+v-container(fluid)
+    // Navigation drawer toggled for small screens
+    v-navigation-drawer(v-model="drawer" right app temporary)
+        v-list
+            v-list-item(v-for="(item, index) in right_menu" :key="index" :to="item.url" nuxt)
+                v-list-item-icon
+                    v-icon(v-text="item.icon")
+                v-list-item-content
+                    v-list-item-title(v-text="item.title")
 
-v-img(:src="src" :alt="alt" transition="slide-y-transition" aspect-ratio=2.5)
-    v-container(fluid)
+    v-app-bar(app dense)
+        v-toolbar-items
+            v-btn(v-for="(item, index) in left_menu" :key="index" :to="item.url" text nuxt x-large)
+                v-icon(left v-text="item.icon")
+                |   {{ item.title }}
+        v-spacer
 
-        v-container.image-overlay
-        p.text-overlay
+        // Small screens and up
+        v-toolbar-items.hidden-sm-and-down
+            v-btn(v-for="(item, index) in right_menu" :key="index" :to="item.url" text nuxt)
+                v-icon(left v-text="item.icon")
+                |   {{ item.title }}
+
+        // Extra small screens
+        v-app-bar-nav-icon.hidden-md-and-up(@click.stop="drawer = !drawer")
 
 </template>
 
 <script>
 
+import generalConfig from '../content/general.json'; // TODO: replace with nuxt content
+
 export default {
-    props: {
-        src: {
-            type: String,
-            default: ''
-        },
-        alt: {
-            type: String,
-            default: ''
-        }
-    }
+    data: () => ({
+        drawer: null,
+        left_menu: [
+            {
+                title: generalConfig.title,
+                url: '/',
+                icon: 'mdi-home'
+            }
+        ],
+        right_menu: [
+            {
+                title: 'Research',
+                url: '/research',
+                icon: 'mdi-feather',
+                sub_menu: [
+                    {
+                        title: 'Published',
+                        url: '/research/published',
+                        icon: 'mdi-feather'
+                    },
+                    {
+                        title: 'Unpublished',
+                        url: '/research/unpublished',
+                        icon: 'mdi-feather'
+                    }
+                ]
+            },
+            {
+                title: 'Portfolio',
+                url: '/portfolio',
+                icon: 'mdi-library-books',
+                sub_menu: [
+                    {
+                        title: 'Projects',
+                        url: '/portfolio/projects',
+                        icon: 'mdi-settings'
+                    },
+                    {
+                        title: 'Art',
+                        url: '/portfolio/art',
+                        icon: 'mdi-brush'
+                    }
+                ]
+            },
+            {
+                title: 'Blog',
+                url: '/blog',
+                icon: 'mdi-book-open',
+                sub_menu: [
+                    {
+                        title: 'Technical',
+                        url: '/blog/technical',
+                        icon: 'mdi-book-open'
+                    },
+                    {
+                        title: 'Personal',
+                        url: '/blog/personal',
+                        icon: 'mdi-book-open'
+                    }
+                ]
+            },
+            {
+                title: 'Reviews',
+                url: '/reviews',
+                icon: 'mdi-film',
+                sub_menu: [
+                    {
+                        title: 'Film',
+                        url: '/reviews/film',
+                        icon: 'mdi-film'
+                    },
+                    {
+                        title: 'Games',
+                        url: '/reviews/games',
+                        icon: 'mdi-film'
+                    }
+                ]
+            }
+        ]
+    })
 };
 
 </script>
 
 <style scoped>
-
-.image-overlay {
-    position: absolute;
-    filter: brightness(40%);
-}
-
-.text-overlay {
-    color: white;
-    filter: blur(4px);
-    -webkit-filter: blur(4px);
-}
 
 </style>
