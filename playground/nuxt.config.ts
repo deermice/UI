4740b9dc784fbf5c23fd61f05f36a77a68304ebe
Asher Norland <asher.norland@gmail.com>
2024-11-12 18:48:01 -0500

    Lints

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
 	modules: ['../src/module'],
-	deermiceComponents: {},
 	devtools: { enabled: true },
-	compatibilityDate: '2024-11-12'
-})
+	compatibilityDate: '2024-11-12',
+	deermiceComponents: {},
+});
