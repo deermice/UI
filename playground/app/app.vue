64d5d9dc8c8753a2b5d0ce7a46a42fffb0d87338
Asher Norland <asher.norland@gmail.com>
2025-03-05 19:29:17 -0500

    Update

--- playground/app/app.vue
+++ playground/app/app.vue
@@ -10,29 +10,5 @@
 </template>
 
 <script setup lang="ts">
-import { useColorMode } from '#imports'
 
-const router = useRouter()
-const appConfig = useAppConfig()
-const colorMode = useColorMode()
-
-const isDark = computed({
-	get() {
-		return colorMode.value === 'dark'
-	},
-	set() {
-		colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
-	}
-})
-
-
-const isCommandPaletteOpen = ref(false)
-
-function onSelect(item: any) {
-	router.push(item.to)
-}
-
-defineShortcuts({
-	meta_k: () => isCommandPaletteOpen.value = true
-})
 </script>
--- playground/app/pages/index.vue
+++ playground/app/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="text-center">
-        <h1 class="font-semibold mb-1">
-            Playground
-        </h1>
-    </div>
+	<div class="text-center">
+		<h1 class="font-semibold mb-1">
+			Playground
+		</h1>
+	</div>
 </template>
